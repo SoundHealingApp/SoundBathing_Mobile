9513a8d27736c373165bc9d07c9ae8ccf26d8fa2
Irina <irinapechik@gmail.com>
2025-02-06 21:37:35 +0300

    initial commit

---
+++ SoundBathingApp/SoundBathingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundBathingAppApp.swift
+//  SoundBathingApp
+//
+//  Created by Ирина Печик on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoundBathingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
