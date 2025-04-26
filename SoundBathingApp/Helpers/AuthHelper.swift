eefe24aa00c93278b779574f4f72b8bbbf74c506
Irina <irinapechik@gmail.com>
2025-04-26 22:49:10 +0300

    refactoring

---
+++ SoundBathingApp/Helpers/AuthHelper.swift
@@ -0,0 +1,15 @@
+//
+//  AuthHelpers.swift
+//  SoundBathingApp
+//
+//  Created by Ирина Печик on 26.04.2025.
+//
+
+import Foundation
+import CryptoKit
+
+func hashPassword(_ password: String) -> String {
+    let data = Data(password.utf8)
+    let hash = SHA256.hash(data: data)
+    return hash.map { String(format: "%02hhx", $0) }.joined()
+}
